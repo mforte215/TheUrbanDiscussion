c79662fe0cf0bebad0d2cd41b1caf679ca2d9d88
MForte101 <mark.fortee@gmail.com>
2022-04-06 22:05:54 -0400

    thread and comments

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Member, Tag, Article
+from .models import Member, Tag, Article, Thread, Comment
 admin.site.register(Member)
 admin.site.register(Tag)
 admin.site.register(Article)
+admin.site.register(Thread)
+admin.site.register(Comment)
--- app/models.py
+++ app/models.py
@@ -47,3 +47,41 @@ class Article(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Article, self).save(*args, **kwargs)
+
+class Thread(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Member, on_delete=CASCADE, related_name="threads")
+    published_date = models.DateTimeField(auto_now_add=True, editable=False)
+    text = RichTextField()
+    slug = models.SlugField(unique=True, db_index=True, blank=True, max_length=255)
+    tags = models.ManyToManyField(Tag)
+    image = models.URLField()
+    date = models.DateTimeField(auto_now_add=True, editable=False, null=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Thread, self).save(*args, **kwargs)
+
+class Comment(models.Model): 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    thread = models.ForeignKey(Thread,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80) 
+    email = models.ForeignKey(
+        Member, on_delete=CASCADE, related_name="user_comments")
+    body = models.TextField() 
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
+    active = models.BooleanField(default=True) 
+
+    class Meta: 
+        ordering = ('created',) 
+
+    def __str__(self): 
+        return 'Comment by {} on {}'.format(self.name, self.post)
